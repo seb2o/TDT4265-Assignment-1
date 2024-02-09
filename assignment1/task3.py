2b93f564a777aaaeabadfde3608e98d4aea18a12
menbu <menbuas69@gmail.com>
2024-02-09 15:00:37 +0100

    added regularization term to loss of the training step

--- assignment1/task3.py
+++ assignment1/task3.py
@@ -39,8 +39,9 @@ class SoftmaxTrainer(BaseTrainer):
         Returns:
             loss value (float) on batch
         """
+        self.model.zero_grad()
         Out_batch = self.model.forward(X_batch)
-        loss = cross_entropy_loss(Y_batch, Out_batch)
+        loss = cross_entropy_loss(Y_batch, Out_batch) + self.model.l2_reg_lambda*np.linalg.norm(self.model.w)**2
         self.model.backward(X_batch, Out_batch, Y_batch)
         self.model.w = self.model.w - self.learning_rate*self.model.grad
         self.model.zero_grad()
